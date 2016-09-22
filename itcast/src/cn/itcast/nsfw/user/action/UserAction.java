026dfec3fd5c910bd8d7efd9ff6e3d3aaed4a914
wxc <wxc2zm@126.com>
2016-09-22 18:10:15 +0800

    第二天1

--- itcast/src/cn/itcast/nsfw/user/action/UserAction.java
+++ itcast/src/cn/itcast/nsfw/user/action/UserAction.java
@@ -1,8 +1,14 @@
 package cn.itcast.nsfw.user.action;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Resource;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.struts2.ServletActionContext;
 
 import cn.itcast.nsfw.user.entity.User;
 import cn.itcast.nsfw.user.service.UserService;
@@ -18,6 +24,9 @@ public class UserAction extends ActionSupport {
 	private List<User> userList;
 	private User user;
 	private String[] selectedRow;
+	private File headImg;
+	private String headImgFileName;
+	private String headImgContentType;
 
 	//列表页面
 	public String listUI(){
@@ -30,9 +39,26 @@ public class UserAction extends ActionSupport {
 	}
 	//保存新增
 	public String add(){
-		if(user != null){
-			userService.save(user);
+		try {
+			if(user != null){
+				//处理头像
+				if (headImg != null) {
+					//1、保存头像到upload/user
+					//获取保存路径的绝对地址
+					String filePath = ServletActionContext.getServletContext().getRealPath("upload/user");
+					String fileName = UUID.randomUUID().toString().replace("-", "") + headImgFileName.substring(headImgFileName.lastIndexOf("."));
+					//复制文件
+					FileUtils.copyFile(headImg, new File(filePath, fileName));
+					
+					//2、设置用户头像路径
+					user.setHeadImg("user/" + fileName);
+				}
+				userService.save(user);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
 		return "list";
 	}
 	//跳转到编辑页面
@@ -82,5 +108,23 @@ public class UserAction extends ActionSupport {
 	}
 	public void setSelectedRow(String[] selectedRow) {
 		this.selectedRow = selectedRow;
+	}
+	public void setHeadImg(File headImg) {
+		this.headImg = headImg;
+	}
+	public File getHeadImg() {
+		return headImg;
+	}
+	public void setHeadImgFileName(String headImgFileName) {
+		this.headImgFileName = headImgFileName;
+	}
+	public String getHeadImgFileName() {
+		return headImgFileName;
+	}
+	public void setHeadImgContentType(String headImgContentType) {
+		this.headImgContentType = headImgContentType;
+	}
+	public String getHeadImgContentType() {
+		return headImgContentType;
 	}
 }
